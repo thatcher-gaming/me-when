86c78c10abb7c7fe8ce8753f6dd5a34407cbbd21
leah <leah@pronounmail.com>
2023-10-12 19:10:44 +0100

    make clippy happy

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,7 @@ const CONFIG_PATH: &str = "config.toml";
 impl BotConfig {
     pub fn load() -> Self {
         let file = fs::read_to_string(CONFIG_PATH)
-            .expect(&format!("Failed to read {CONFIG_PATH}"));
+            .unwrap_or_else(|_| panic!("Failed to read {CONFIG_PATH}"));
 
         toml::from_str(&file).expect("Failed to parse config")
     }
